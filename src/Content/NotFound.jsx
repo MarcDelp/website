dede9c46cb71034c9110540aa131d88503ade8b4
Marc Delpech <marc.delpech@telecomnancy.net>
2021-08-27 23:27:58 +0200

    fix(404): update text error

--- src/Content/NotFound.jsx
+++ src/Content/NotFound.jsx
@@ -5,7 +5,7 @@ export default function NotFound () {
   return (
     <div>
       <h1>404</h1>
-      <p>Il semblerait que nous ne soyons pas en mesure d&apos;afficher cette page... Existe-t&apos;elle réellement ?</p>
+      <p>Il semblerait que nous ne soyons pas en mesure d&apos;afficher cette page... Existe-t-elle réellement ?</p>
       <p><Link href='/'><strong>Retourner à l&apos;accueil</strong></Link></p>
     </div>
   )
